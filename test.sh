d35d59a7f5cd95732f62fbbcf264f8e9bd733f38
Chris Ellis <crellis17@gmail.com>
2023-02-23 01:03:39 -0500

    Cleaup test.sh

--- test.sh
+++ test.sh
@@ -4,22 +4,20 @@ set -o pipefail
 
 ERRORS=()
 
-# find all executables and run `shellcheck`
 
-while IFS= read -r -d '' infile
-do
-	if file "$infile" | grep --quiet shell; then
+# run shellcheck on executables
+for f in $(find . -type f -not -path '*.git*' -not -name "README.md" | sort -u); do
+	if file "$f" | grep --quiet shell; then
 		{
-			shellcheck -x "$infile" && echo -e "\\e[32m[OK]: linted $infile\\e[0m"
+			shellcheck "$f" && echo "[OK]: successfully linted $f"
 		} || {
-			# add to errors
-			ERRORS+=("$infile")
+			ERRORS+=("$f")
 		}
 	fi
-done <   <(find . -type f -not -path '*.git*' -not -name "README.md" -print0 | sort -u)
+done
 
 if [ ${#ERRORS[@]} -eq 0 ]; then
-	echo "No errors in tests"
+	echo "No errors"
 else
 	echo -e "\\e[31mThese files failed shellcheck: ${ERRORS[*]}\\e[0m"
 	exit 1
